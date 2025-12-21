6b8cfbfa3fe52ae5abdc698390f3ab6ff177c8d3
mateo-vasquez999 <mateo.vasqueze9@gmail.com>
2025-12-20 23:14:46 -0500

    Update telegram_bot.py 3

--- telegram_bot.py
+++ telegram_bot.py
@@ -240,7 +240,7 @@ def main():
     # 📚 Examen inicial — 1:00 PM
     app.job_queue.run_daily(
         daily_job,
-        time=time(hour=23, minute=16, tzinfo=LOCAL_TZ),
+        time=time(hour=23, minute=19, tzinfo=LOCAL_TZ),
         days=(0, 1, 2, 3, 4, 5, 6)
     )
 
