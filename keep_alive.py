960f5d2cecd34bd636bac014808a191b872889f6
mateo-vasquez999 <mateo.vasqueze9@gmail.com>
2025-12-27 19:57:17 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,6 +9,10 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Bot is running")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
 def start_server():
     server = HTTPServer(("0.0.0.0", PORT), Handler)
     server.serve_forever()
